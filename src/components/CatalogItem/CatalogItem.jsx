aca2494bc433c1e1d774f55a1d0d3813c22c4068
kiianskii <ankiyanskiy@gmail.com>
2024-05-14 17:45:33 +0300

    issue with svg

--- src/components/CatalogItem/CatalogItem.jsx
+++ src/components/CatalogItem/CatalogItem.jsx
@@ -3,9 +3,9 @@ import { useToggle } from "../../hooks/useToggle";
 import { changeFavorite } from "../../redux/cars/slice";
 import Modal from "../Modal/Modal";
 import s from "./CatalogItem.module.css";
-import Icons from "../../assets/sprite.svg";
 import { useEffect } from "react";
-import { Icon } from "../../assets/Icon";
+// import Icons from "../../assets/sprite.svg";
+// import { Icon } from "../../assets/Icon";
 
 function CatalogItem({ item }) {
   const dispatch = useDispatch();
@@ -33,30 +33,29 @@ function CatalogItem({ item }) {
     favorite,
   } = item;
 
-  // const favoriteIcon = favorite ? "#icon-active" : "#icon-normal";
   return (
     <li className={s.item}>
       <label className={s.label}>
         <input
           id="favorite"
-          className={s.checkbox + " " + s.visually_hidden}
+          className={s.checkbox}
           checked={favorite}
           type="checkbox"
           onChange={() => dispatch(changeFavorite(item.id))}
         />
-        <span className={s.heart_span}>
+        {/* <span className={s.heart_span}>
           {favorite ? (
-            // <svg width={18} height={18} className={s.heart}>
-            //   <use href={Icons + "#icon-active"}></use>
-            // </svg>
+            <svg width={18} height={18} className={s.heart}>
+              <use href={Icons + "#icon-active"}></use>
+            </svg>
             <Icon size={18} id="active" className={s.heart} />
           ) : (
-            // <svg width={18} height={18} className={s.heart}>
-            //   <use href={Icons + "#icon-normal"}></use>
-            // </svg>
+            <svg width={18} height={18} className={s.heart}>
+              <use href={Icons + "#icon-normal"}></use>
+            </svg>
             <Icon size={18} id="normal" className={s.heart} />
           )}
-        </span>
+        </span> */}
       </label>
       <>
         <img className={s.img} src={img} alt={model} width="274" height="268" />
